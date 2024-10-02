0c1e0fe35212aab2ea447cf71c869cdc2800632e
FOSSMan <domi382@outlook.com>
2024-10-02 19:48:22 +0200

    added under_construction.html, removed uptime link, added darkmodelabel

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -2,10 +2,11 @@ function toggleDarkMode() {
     if($('body').hasClass('dark-mode')) {
         $('body').addClass('light-mode');
         $('body').removeClass('dark-mode');
-        $('darkModeButton').text = "Light Mode";
+        $('darkModeLabel').text = "Light Mode";
     } else {
         $('body').addClass('dark-mode');
         $('body').removeClass('light-mode');
-        $('darkModeButton').text = "Dark Mode";
+        $('darkModeLabel').text = "Dark Mode";
     }
 }
+//TODO Fix Label change
